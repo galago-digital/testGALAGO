06a5068da07aaa139ffadb5d987c6e7b18e16527
nikitaerman <68640433+nikitaerman@users.noreply.github.com>
2020-08-25 18:05:43 +0500

    Add files via upload

---
+++ jquery/date.js
@@ -0,0 +1,26 @@
+$("#date_birth").datepicker({
+    numberOfMonths: 1,
+    changeYear: true,
+    changeMonth: true,
+    minDate: "-36525D",
+    maxDate: 0,
+});
+
+$.datepicker.regional['ru'] = {
+    closeText: 'Закрыть',
+    prevText: 'Предыдущий',
+    nextText: 'Следующий',
+    currentText: 'Сегодня',
+    monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+    dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+    dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+    dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    weekHeader: 'Не',
+    dateFormat: 'dd.mm.yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ''
+};
+$.datepicker.setDefaults($.datepicker.regional['ru']);
